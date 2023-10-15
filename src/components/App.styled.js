b3322b8c42e15e3784ffe381d21606855f76bed7
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-10-15 11:38:01 +0300

    fix deploy

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -2,9 +2,4 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   padding: 0 15px;
-
-  /* @media screen and (max-width: 575px) {
-    width: 575px;
-    background-color: gray;
-  } */
 `;
